2a17d79592f71d7db1219caad2834ea6b3fef92c
FagnerFerreira <ferreirabagundes@gmail.com>
2025-04-20 01:26:07 -0300

    animação para os gifs

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,31 @@
 const compreTxt = document.querySelector(`#compre`)
-
+const ghost = document.querySelector(`.ghost-gif`)
+const baloes = document.querySelector(`.baloes-gif`)
 const pl = [`Venda`, `Troque`, `Compre`]
 let i = 0
 
 
+function subirDescer(valor) {
+    let bottom = getComputedStyle(ghost).marginBottom
+    let top = getComputedStyle(baloes).marginTop
+    
+    
+
+    ghost.style.marginBottom = valor
+    baloes.style.marginTop = `0px`
+
+    
+    if (bottom === valor ) ghost.style.marginBottom = `0px`
+    if (top < valor ) baloes.style.marginTop = valor
+    console.log(baloes);
+    
+    
+}
 const trocaDePalavras = setInterval(() => {
     compreTxt.innerHTML = pl[i]
     i < 2 ? i++  : i = 0
 }, 1000);
+
+
+setInterval(() => subirDescer(`200px`), 2000);
 

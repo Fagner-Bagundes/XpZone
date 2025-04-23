5ec44f3176357a3ac6778abb8422a12a5de38f6b
FagnerFerreira <ferreirabagundes@gmail.com>
2025-04-23 01:15:52 -0300

    interação do box-city feita

--- src/js/cetasDeSelecao.js
+++ src/js/cetasDeSelecao.js
@@ -1,14 +1,25 @@
 const selecaoArea = document.querySelector(`.box-city`)
 const citys = document.querySelector(`.citys`)
 const city = document.querySelectorAll(`.city`)
+const cetaBottom = document.querySelector(`.ceta-bottom`)
 
 export function caixaSelecao() {
     selecaoArea.addEventListener(`click`, ()=>{
-        citys.classList.add(`citys-active`)
-        city.forEach(element => {
-            element.classList.add(`city-active`)
-            
-        });
+
+        let citysClass = citys.classList.contains(`citys-active`)
+        if (citysClass) {
+            citys.classList.remove(`citys-active`)
+            cetaBottom.classList.remove(`ceta-bottom-active`)
+        } else{
+            citys.classList.add(`citys-active`)
+            city.forEach(element => {
+                element.classList.add(`city-active`)
+            });
+
+            cetaBottom.classList.add(`ceta-bottom-active`)
+
+        }
+        
 
     })
 }
